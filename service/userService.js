02a8f80467602996529de868a4145b5777bd25c4
M Haseeb Shahzad <hs0085747@gmail.com>
2023-10-19 12:24:04 +0500

    done with the LMS task

--- service/userService.js
+++ service/userService.js
@@ -1,5 +1,5 @@
 const { models } = require("../models/definitions");
-
+const bcrypt = require("bcrypt")
 module.exports = {
   getUsers: async () => {
     const result = await models.user.findAll();
